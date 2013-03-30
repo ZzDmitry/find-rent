89b9106770594d8cb0337a23a12952f9776dc080
ZzDmitry <ZzDmitry@yandex.ru>
2013-03-30 22:42:09 +0700

    Extract link and desc texts

--- 1.js
+++ 1.js
@@ -31,20 +31,49 @@ getForumPage(function(err, text){
 		console.log(err);
 		return;
 	}
-	console.log(extractForumPageTopicsAsTexts(text));
+	var topics_texts = extractForumPageTopics(text);
+	var i;
+	for (i = 0; i < topics_texts.length; i++) {
+		console.log('-------------------------');
+		console.log(topics_texts[i]);
+		console.log(parseTopicHeaderText(topics_texts[i]));
+	}
 });
 
 /**
  * Parse forum page as topics.
  * @param {String} text
- * @return {Array.<String>}
+ * @return {Array.<{id: String, text: String}>}
  */
-function extractForumPageTopicsAsTexts(text) {
+function extractForumPageTopics(text) {
 	var re = /<!-- Begin Topic Entry (\d+) -->([\s\S]*?)<!-- End Topic Entry \1 -->/g;
 	var match;
 	var matches = [];
 	while (match = re.exec(text)) {
-		matches.push([match[1], match[2]])
+		matches.push({id:match[1], text:match[2]})
 	}
 	return matches;
+}
+
+/**
+ * @param {String} text
+ * @param {{tag: String, id: String}} element
+ * @return {String}
+ */
+function parseElementContent(text, element) {
+	var re = new RegExp('<' + element.tag + '\\s[^>]*id=("|\')' + element.id + '\\1[^>]*?>([\\s\\S]*?)<', '');
+	var match = re.exec(text);
+	return match ? match[2] : '';
+}
+
+/**
+ *
+ * @param {{id: String, text: String}} header
+ * @return {Object}
+ */
+function parseTopicHeaderText(header) {
+	return {
+		link: parseElementContent(header.text, {tag: 'a', id: 'tid-link-' + header.id}),
+		desc: parseElementContent(header.text, {tag: 'span', id: 'tid-desc-' + header.id})
+	};
 }
